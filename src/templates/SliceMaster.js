d190998f245b077caf2f0fc6969dd80e9053b8d6
binarytrance <ganeshan.dash@gmail.com>
2021-07-22 21:05:35 +0530

    added seo to all components

--- src/templates/SliceMaster.js
+++ src/templates/SliceMaster.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import SEO from '../components/SEO';
 
-const SliceMaster = ({ data: { sliceMaster }, pageContext }) => {
+const SliceMaster = ({ data: { sliceMaster } }) => {
   console.log(sliceMaster);
   return (
-    <div className="center">
-      <Img fluid={sliceMaster.image.asset.fluid} />
-      <h2>
-        <span className="mark">{sliceMaster.name}</span>
-      </h2>
-      <p>{sliceMaster.description}</p>
-    </div>
+    <>
+      <SEO
+        title={`Slice Master - ${sliceMaster.name}`}
+        image={sliceMaster.image?.asset?.fluid.src}
+      />
+      <div className="center">
+        <Img fluid={sliceMaster.image.asset.fluid} />
+        <h2>
+          <span className="mark">{sliceMaster.name}</span>
+        </h2>
+        <p>{sliceMaster.description}</p>
+      </div>
+    </>
   );
 };
 
