5bc90972efec112769bccecda85c19ab20b3074a
binarytrance <ganeshan.dash@gmail.com>
2021-08-23 23:45:28 +0530

    wrote useLatestData which fetches data on the client side

---
+++ src/customHooks/useLatestData.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+export default function useLatestData() {
+  // hot slices
+  const [hotSlices, setHotSlices] = useState();
+  // slicemasters
+  const [sliceMasters, setSliceMasters] = useState();
+
+  // use a sideeffect to fetch data from the graphql endpoint
+  useEffect(() => {
+    // when the component loads, fetch data
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query{
+            StoreSettings(id: "koramangala") {
+              name
+              slicemasters {
+                name
+              }
+              hotSlices {
+                name
+              }
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // TODO: set states
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSliceMasters(res.data.StoreSettings.slicemasters);
+      });
+  }, []);
+
+  return [hotSlices, sliceMasters];
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import useLatestData from '../customHooks/useLatestData';
+
+function CurrentlySlicing() {
+  return <p>CurrentlySlicing</p>;
+}
+function HotSlices() {
+  return <p>HotSlices</p>;
+}
 
 export default function Homepage() {
+  const [hotSlices, sliceMasters] = useLatestData();
+  console.log(hotSlices, sliceMasters);
+
   return (
     <>
-      <div>Hey, World!</div>
+      <div className="center">
+        <h1>Most scrumptious saucers in the world!</h1>
+        <p>Open 24/7</p>
+        <div>
+          <CurrentlySlicing />
+          <HotSlices />
+        </div>
+      </div>
     </>
   );
 }
