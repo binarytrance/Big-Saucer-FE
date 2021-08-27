fd95c92e76c2008342a90941142e4dfb521bbd5a
binarytrance <ganeshan.dash@gmail.com>
2021-08-28 02:27:20 +0530

    removed import dotenv import from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
+// dotenv.config({ path: '.env' });
 
 export default {
   siteMetadata: {
